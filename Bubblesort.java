a22f45eaf116d9c6f27156f5f8af9f3335264d94
IslantySeniUtami <46374176+IslantySeniUtami@users.noreply.github.com>
2019-02-21 09:10:30 +0700

    Add files via upload

---
+++ Bubblesort.java
@@ -0,0 +1,14 @@
+package bubblesort;
+public class Bubblesort {
+    public void bubblessort() {
+        int n = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = n - 1; j >= 1; j--) { if (data[j] < data[j - 1]) {
+                    tukar(j, j - 1); // ascending 
+                }
+            }
+        }
+        
+    }
+    
+}
